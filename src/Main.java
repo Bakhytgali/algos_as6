2e1962775797fa19e4e853f4ad6083511d5d7fab
Your Name <omgwtfwat@gmail.com>
2023-05-29 12:10:50 +0600

    tests for addVertex() and addEdge() functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        MyGraph<Integer> gr = new MyGraph<>();
+        gr.addVertex(1);
+        gr.addVertex(3);
+        gr.addVertex(2);
+        gr.addVertex(4);
+        gr.addEdge(1, 3);
+        gr.addEdge(1,2);
+        gr.addEdge(2,3);
+        gr.addEdge(3,4);
     }
 }
